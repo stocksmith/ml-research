99489b3e2e049710a26ceac642bb9c0d2940fa99
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-23 02:49:08 -0500

    minor

--- real-time-data/app.go
+++ real-time-data/app.go
@@ -2,27 +2,36 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/piquette/finance-go/chart"
-	"github.com/piquette/finance-go/datetime"
-	"github.com/piquette/finance-go/quote"
 )
 
 func main() {
-	
+	historic_quotes()
 }
 
-func historic_quotes() {
-	params := &chart.Params{
-		Symbol:   "TWTR",
-		Interval: datetime.OneHour,
-	}
-	iter := chart.Get(params)
+func quote() {
 
-	for iter.Next() {
-		fmt.Println(iter.Bar())
+	q, err := quote.Get("AAPL")
+	if err != nil {
+		// Uh-oh.
+		panic(err)
 	}
-	if err := iter.Err(); err != nil {
-		fmt.Println(err)
-	}
+
+	// Success!
+	fmt.Println(q)
+
 }
+
+// func historic_quotes() {
+// 	params := &chart.Params{
+// 		Symbol:   "TWTR",
+// 		Interval: datetime.OneHour,
+// 	}
+// 	iter := chart.Get(params)
+
+// 	for iter.Next() {
+// 		fmt.Println(iter.Bar())
+// 	}
+// 	if err := iter.Err(); err != nil {
+// 		fmt.Println(err)
+// 	}
+// }
