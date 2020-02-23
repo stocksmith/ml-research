55b5fc703abd2d13358ee035b75ba1fa633b145e
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-22 23:01:10 -0500

    added more functionality

---
+++ database-agg/engine.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+a = pd.read_csv(filepath_or_buffer = "../datasets/a.csv")
+print(a)
--- database-agg/firebase.py
+++ database-agg/firebase.py
@@ -5,18 +5,19 @@ from firebase_admin import firestore
 # Use a service account
 cred = credentials.Certificate('stocksmith.json')
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
-doc_ref = db.collection(u'users').document(u'alovelace')
-doc_ref.set({
-    u'first': u'Ada',
-    u'last': u'Lovelace',
-    u'born': 1815
-})
 
 
-# users_ref = db.collection(u'users')
-# docs = users_ref.stream()
+# doc_ref = db.collection(u'users').document(u'alovelace')
+# doc_ref.set({
+#     u'first': u'Ada',
+#     u'last': u'Lovelace',
+#     u'born': 1815
+# })
 
-# for doc in docs:
-#     print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
+users_ref = db.collection(u'datasets')
+docs = users_ref.stream()
+
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
