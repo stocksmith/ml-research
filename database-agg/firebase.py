caa0c13570969fe159d9f78bed5d66c83f2099a0
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-22 22:54:15 -0500

    minor

--- database-agg/firebase.py
+++ database-agg/firebase.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use a service account
-cred = credentials.Certificate('path/to/serviceAccount.json')
+cred = credentials.Certificate('stocksmith.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
