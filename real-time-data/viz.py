b18985879d0d9190f3a5d03c8be6067d749d7693
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-23 01:30:57 -0500

    minor

--- real-time-data/viz.py
+++ real-time-data/viz.py
@@ -2,6 +2,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pylab as pl
 import pandas as pd
+import numpy as np
+from scipy.signal import find_peaks
+
 matplotlib.style.use('ggplot')
 
 msft = pd.read_csv(filepath_or_buffer = "msft.csv")
@@ -12,6 +15,23 @@ data_open = []
 for index, row in msft.iterrows():
     timestamps.append(row['Date'])
     data_open.append(row['Open'])
+
+
+test = find_peaks(data_open)
+
+peaks = test[0]
+
+peak_arr = np.zeros(len(data_open))
+
+# peaks
+x = []
+# timestamps for those peaks
+y = []
+
+for i in peaks:
+    peak_arr[i] = data_open[i]
+    x.append(data_open[i])
+    y.append(data_open[i])
 
 plt.figure(figsize=(10,5))
 pl.plot(timestamps, data_open, 'r-',color="red",label='unfiltered')
