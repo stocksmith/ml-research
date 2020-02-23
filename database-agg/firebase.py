0ec508f8f1a50e3ce6ec4db6f38855b5cde6926c
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-22 22:48:08 -0500

    firebase py

---
+++ database-agg/firebase.py
@@ -0,0 +1,22 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('path/to/serviceAccount.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1815
+})
+
+
+users_ref = db.collection(u'users')
+docs = users_ref.stream()
+
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
