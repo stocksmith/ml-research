6939f4b8d63914ff98c0a55273e05d273bdc113a
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-22 21:48:14 -0500

    minor

--- r-analysis/initial.r
+++ r-analysis/initial.r
@@ -2,3 +2,11 @@
 a <- read.csv("../datasets/A.csv")
 dim(a)
 head(a, 5)
+
+plot(a$date, a$open, xlab="dates", ylab="stock price-opening")
+plot(a$date, a$close, xlab="dates", ylab="stock price-closing")
+
+fit.1 <- lm(close ~ open, data=a) 
+summary(fit.1)
+coef(fit.1)
+
