094738877b2926aaf71b720cb9839a71c80295df
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-23 02:33:15 -0500

    aaol

---
+++ r-analysis/aaol.r
@@ -0,0 +1,22 @@
+# Analysis
+a <- read.csv("../datasets/AAOL.csv")
+dim(a)
+head(a, 5)
+
+plot(a$date, a$open, xlab="dates", ylab="stock price-opening")
+plot(a$date, a$close, xlab="dates", ylab="stock price-closing")
+
+fit.1 <- lm(close ~ open, data=a) 
+summary(fit.1)
+coef(fit.1)
+
+a.gg <- a[, c("volume", "high", "low", "open", "close", "adjclose")]
+ggpairs(data=a.gg)
+
+
+test.data <- a[1,c("close", "open")]
+pred.1 <- predict(fit.1, test.data, interval = "prediction", level = 0.95)
+summary(pred.1)
+# Subtracting actual value from the linear model prediction
+residual <- test.data$close - pred.1[,1]
+print(residual)
