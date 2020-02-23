67419264d14d8d289c277be8a535321c5650d9f4
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-23 00:04:30 -0500

    setup file update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Basic Libraries 
-pip3 install numpy
-pip3 install pandas
-pip3 install matplotlib
-pip3 install scipy
+sudo pip3 install numpy
+sudo pip3 install pandas
+sudo pip3 install matplotlib
+sudo pip3 install scipy
 
