47a9fa7b55284d240b76afa0d17c575ab10da649
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-23 01:29:29 -0500

    ploting microsoft stock

--- real-time-data/viz.py
+++ real-time-data/viz.py
@@ -1,15 +1,20 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import pylab as pl
-import pandas pd
+import pandas as pd
 matplotlib.style.use('ggplot')
 
-msft = pd.read_csv(filepath_or_buffer = "../datasets/msft.csv")
+msft = pd.read_csv(filepath_or_buffer = "msft.csv")
 
-for index, row in a.iterrows():
+timestamps = []
+data_open = []
+
+for index, row in msft.iterrows():
+    timestamps.append(row['Date'])
+    data_open.append(row['Open'])
 
 plt.figure(figsize=(10,5))
-pl.plot(timestamps, magnitude_result, 'r-',color="red",label='unfiltered')
-plt.title("Three Axis Acceleration")
+pl.plot(timestamps, data_open, 'r-',color="red",label='unfiltered')
+plt.title("MSFT Stock")
 pl.grid()
 pl.show()
