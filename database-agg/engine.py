2ce570619977aec5ac8c83976d366a5727c6c227
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-22 23:33:18 -0500

    minor

--- database-agg/engine.py
+++ database-agg/engine.py
@@ -1,4 +1,33 @@
+# Pandas
+import datetime
 import pandas as pd
 
-a = pd.read_csv(filepath_or_buffer = "../datasets/a.csv")
-print(a)
+# Initialize fireabase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('stocksmith.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+temp = {}
+temp['date'] = 2018-12-28
+temp['open'] = 65.45
+i_id = 0
+doc_ref = db.collection(u'datasets').document(u'test')
+doc_ref.set({
+    u'hello': temp
+}, merge=True)
+
+
+# a = pd.read_csv(filepath_or_buffer = "../datasets/a.csv")
+
+# for index, row in a.iterrows():
+#     try: 
+#         print(index)
+#         datetime_object = datetime.datetime.strptime(row['date'], '%Y-%m-%d')
+#         print(datetime_object)
+#     except:
+#         print("An exception occurred")
