e62dfcaf781dae8833a3c481af2a26333bbb7141
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-23 02:47:48 -0500

    app.go

--- real-time-data/app.go
+++ real-time-data/app.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/piquette/finance-go/chart"
+	"github.com/piquette/finance-go/datetime"
+	"github.com/piquette/finance-go/quote"
 )
 
 func main() {
-	q, err := quote.Get("AAPL")
-	if err != nil {
-		// Uh-oh.
-		panic(err)
-	}
-
-	// Success!
-	fmt.Println(q)
+	
 }
 
-func quote() {
+func historic_quotes() {
+	params := &chart.Params{
+		Symbol:   "TWTR",
+		Interval: datetime.OneHour,
+	}
+	iter := chart.Get(params)
 
+	for iter.Next() {
+		fmt.Println(iter.Bar())
+	}
+	if err := iter.Err(); err != nil {
+		fmt.Println(err)
+	}
 }
