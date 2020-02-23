9d0aa9511993c9e3e0c979e2a536be628f80296c
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-23 02:49:46 -0500

    testing chart making with go

--- real-time-data/app.go
+++ real-time-data/app.go
@@ -2,36 +2,39 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/piquette/finance-go/chart"
+	"github.com/piquette/finance-go/datetime"
 )
 
 func main() {
 	historic_quotes()
 }
 
-func quote() {
+// func quote() {
 
-	q, err := quote.Get("AAPL")
-	if err != nil {
-		// Uh-oh.
-		panic(err)
-	}
-
-	// Success!
-	fmt.Println(q)
-
-}
-
-// func historic_quotes() {
-// 	params := &chart.Params{
-// 		Symbol:   "TWTR",
-// 		Interval: datetime.OneHour,
+// 	q, err := quote.Get("AAPL")
+// 	if err != nil {
+// 		// Uh-oh.
+// 		panic(err)
 // 	}
-// 	iter := chart.Get(params)
 
-// 	for iter.Next() {
-// 		fmt.Println(iter.Bar())
-// 	}
-// 	if err := iter.Err(); err != nil {
-// 		fmt.Println(err)
-// 	}
+// 	// Success!
+// 	fmt.Println(q)
+
 // }
+
+func historic_quotes() {
+	params := &chart.Params{
+		Symbol:   "TWTR",
+		Interval: datetime.OneHour,
+	}
+	iter := chart.Get(params)
+
+	for iter.Next() {
+		fmt.Println(iter.Bar())
+	}
+	if err := iter.Err(); err != nil {
+		fmt.Println(err)
+	}
+}
