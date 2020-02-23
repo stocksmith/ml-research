5e9c8d36e5343f2b917b09d3e1663045ccdf3574
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-23 03:13:49 -0500

    added a way to get all names from text file

--- data-agg/names.py
+++ data-agg/names.py
@@ -2,10 +2,13 @@ import pandas as pd
 # data = pd.read_csv('../datasets/NASDAQ.txt', sep=" ", header=None)
 data = pd.read_fwf('../datasets/NASDAQ.txt')
 
-company
+company_list = []
 
 for index, row in data.iterrows():
     print(index)
     str1 = row[0]
     str2 = str1.split()
-    print(str2[0])
+    company_list.append(str2[0])
+
+print("success")
+print(company_list)
