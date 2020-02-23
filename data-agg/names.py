981fbd67473e567ee6ccce6922bddffc6426736d
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2020-02-23 03:11:52 -0500

    minor

--- data-agg/names.py
+++ data-agg/names.py
@@ -1,4 +1,11 @@
 import pandas as pd
 # data = pd.read_csv('../datasets/NASDAQ.txt', sep=" ", header=None)
-data = pd.read_fwf('../datasets/output_list.txt')
-print(data)
+data = pd.read_fwf('../datasets/NASDAQ.txt')
+
+company
+
+for index, row in data.iterrows():
+    print(index)
+    str1 = row[0]
+    str2 = str1.split()
+    print(str2[0])
